123670840c18bbd03c84845bd0e3e41f24c93011
Mateus Oliveira <devmateusoliveira@gmail.com>
2019-03-21 06:26:02 -0300

    parsing metada start

--- parser.rb
+++ parser.rb
@@ -14,9 +14,18 @@ ARGV.each do |arg|
   end
 
   puts '### Tweet Text'
-  doc.css('li div div div p', 'TweetTextSize TweetTextSize--normal js-tweet-text tweet-text').each do |page|
+  selector = 'TweetTextSize TweetTextSize--normal js-tweet-text tweet-text'
+  doc.css('li div div div p', selector).each do |page|
     info.push(page.content.strip)
   end
-#   meta.to_set.each do |data| puts data end
-  puts meta.to_set
+  #   meta .each do |data| puts data end
+  #puts meta 
+  #puts info 
+
+  puts 'Getting Time'
+  doc.css('li div div div a', "tweet-timestamp js-permalink js-nav js-tooltip").each do |atag|
+    puts atag.map{ |link, content| puts "#{link}=\"#{content}\"" }
+  end
 end
+
+
