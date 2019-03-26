ab32554009fbaaf323b9727e3acd1e759c447c2c
Mateus Oliveira <devmateusoliveira@gmail.com>
2019-03-26 07:20:47 -0300

    new parsing way

--- parser.rb
+++ parser.rb
@@ -7,12 +7,13 @@ ARGV.each do |arg|
   doc = Nokogiri::HTML(File.read("info_#{user}.html"))
   meta = []
   info = []
+  meta_ = []
   ## Tweet feito por usuario X
   puts '### Parse user name user @ and replys, retweets, likes'
   doc.css('li div div div span', 'UserBadges').each do |page|
     meta.push(page.content.strip)
   end
-
+  
   puts '### Tweet Text'
   selector = 'TweetTextSize TweetTextSize--normal js-tweet-text tweet-text'
   doc.css('li div div div p', selector).each do |page|
@@ -24,8 +25,18 @@ ARGV.each do |arg|
 
   puts 'Getting Time'
   doc.css('li div div div a', "tweet-timestamp js-permalink js-nav js-tooltip").each do |atag|
-    puts atag.map{ |link, content| puts "#{link}=\"#{content}\"" }
+    info = ""
+    i = 0
+    for link, content in atag do
+      if "#{link}" == "href"  
+        if "#{content}".match(/\/[A-Za-z0-9_]+$/) and not ("#{content}".match(/\/[A-Za-z0-9_]+\/[A-Za-z0-9_]+\/[A-Za-z0-9_]+$/) or "#{content}".match(/https/) ) 
+         puts "match"
+         puts "#{link}=\"#{content}\""
+      end
+      end
+    end
   end
+  #meta_.each{|elem| puts "elment \n #{elem}\n"}
 end
 
 
